e80abc36096eb50775a19c5e8558faf4321c3f06
Micah Banschick <micha.ban@gmail.com>
2020-12-12 21:19:41 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201212203458) do
+ActiveRecord::Schema.define(version: 20201212211922) do
 
   create_table "appointments", force: :cascade do |t|
     t.string   "appointment_datetime"
