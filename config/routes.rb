bcae0ed571b7487da51b790f8e35a5c4d5021860
Micah Banschick <micha.ban@gmail.com>
2020-12-12 20:24:42 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'doctors/index'
+
+  get 'doctors/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
