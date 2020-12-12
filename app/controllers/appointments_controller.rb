5ce8388cfa530adba8642bbc9da61a5cd00b5026
Micah Banschick <micha.ban@gmail.com>
2020-12-12 20:42:11 +0000

    Automatically backed up by Learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,13 @@
 class AppointmentsController < ApplicationController
+
+  def show
+    @appointment =
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:date, :time)
+  end
+
 end
