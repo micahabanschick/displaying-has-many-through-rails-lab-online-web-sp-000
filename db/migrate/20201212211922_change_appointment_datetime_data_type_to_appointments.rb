80d10f69b8b1c11636267070a13b379f7c2a699f
Micah Banschick <micha.ban@gmail.com>
2020-12-12 21:34:19 +0000

    Done.

---
+++ db/migrate/20201212211922_change_appointment_datetime_data_type_to_appointments.rb
@@ -0,0 +1,5 @@
+class ChangeAppointmentDatetimeDataTypeToAppointments < ActiveRecord::Migration[5.0]
+  def change
+    change_column :appointments, :appointment_datetime, :datetime
+  end
+end
---
+++ db/migrate/20201212212545_change_appointment_datetime_data_type_to_datetime.rb
@@ -0,0 +1,5 @@
+class ChangeAppointmentDatetimeDataTypeToDatetime < ActiveRecord::Migration[5.0]
+  def change
+    change_column :appointments, :appointment_datetime, :datetime
+  end
+end
